b4b19793c26d77659a337d66f551bb9f6768b016
Drew Easley <DEasley@rallydev.com>
2014-12-22 11:56:51 -0500

    deasley locked 12

--- status.sh
+++ status.sh
@@ -1,6 +1,6 @@
 git pull
 
-echo "Current status of test clusters <br/>" > README.MD
+echo "<b>Current status of test clusters</b> <br/>" > README.MD
 for f in cluster.*; do
   whoOwns=$(cat $f)
   echo "$f $whoOwns <br/>" >> README.MD
